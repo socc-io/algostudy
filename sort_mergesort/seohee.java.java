ea99183583616274e08d92a39aaa2a356f3f8855
seohee220 <94kimsh@naver.com>
2015-01-30 10:53:24 +0900

    merge sort

--- sort_mergesort/seohee.java.java
+++ sort_mergesort/seohee.java.java
@@ -1,27 +1,24 @@
-package socc;
+package sort;
 
 public class mergesort {
 	public static void main(String[] args){
 		int[] a = {5,3,4,2,1,9,7,6};
-		ms(a,0,7);
-		int[] a1={1,3,5};
-		int[] a2 ={2,4,6};
-		
-		merge2(a1,a2);
-		
+		int[] c=ms(a,0,7);
+		for(int i=0; i<c.length; i++){
+			System.out.print(c[i]);
+		}
 	}
 	
 	public static int[] ms(int[] a, int s, int e){
 		
-		if(s==e || e==s+1){
+		if(s==e ){
 			int[] b = {a[s]};
 			return b;
 		}
 		
 		else{
-			int[] a1 = ms(a, s, e/2);
-			System.out.println(s+" "+e/2);
-			int[] a2 = ms(a, e/2, e);
+			int[] a1 = ms(a, s, (s+e)/2);
+			int[] a2 = ms(a, ((s+e)/2)+1, e);
 			return merge2(a1,a2);
 		}
 		
@@ -31,41 +28,35 @@ public class mergesort {
 		int len_i=a1.length;
 		int len_j=a2.length;
 		
-		int[] c = new int[8];
+		int[] c = new int[len_i+len_j];
 		int i=0, j=0, k=0;
 		
-		while(k<len_i+len_j){
-			/*
+		while(k<8){
+			
 			if(i==len_i){
-				
-				c[k]=a2[j]; j++;
+				for(int a=j; a<len_j; a++){
+					c[k]=a2[a];
+					k++;
+				}
 				break;
 			}
-			if( j==len_j){
-				break;
-			}
-			*/
-			if(k==len_i+len_j-1){
-				if(i<j){ c[k]=a1[i];}
-				else{ c[k]=a2[j];}
-
+			if(j==len_j){
+				for(int a=i; a<len_i; a++){
+					c[k]=a1[a];
+					k++;
+				}
 				break;
 			}
 			
 			if(a1[i]<=a2[j]){
 				c[k]=a1[i];
-				k++;
-				i++;
+				k++; i++;
 			}
 			else{
 				c[k]=a2[j];
-				k++;
-				j++;
+				k++; j++;
 			}
 			
-		}
-		for(int n=0; n<len_i+len_j; n++){
-			System.out.println(c[n]);
 		}
 		return c;
 	}
