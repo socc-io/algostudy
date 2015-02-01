6463ffbceeaefbbd69d0dde5ccfb6644ffd468bc
BaekSeoHyeon <whitewest87@gmail.com>
2015-02-01 17:53:33 +0900

    그렇다 시퀀스를 찾는거였
    하지만 리스트로 검색하지 않고 갯수로만 찾는 방법이 있었음.

---
+++ 줄세우기/becxer.py
@@ -0,0 +1,11 @@
+n = int(raw_input())
+ls = map(int,str(raw_input()).split())
+
+seq_list = [0] * 1000001
+max = 0
+for i in range(n):
+	seq_list[ls[i]] = seq_list[ls[i]-1] + 1
+	if max < seq_list[ls[i]]
+		max = seq_list[ls[i]]
+
+print str(n-max)
