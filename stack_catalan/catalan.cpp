ef182825453a29ff50fe526b87b864ee09a1eec0
minji <minjji410@gmail.com>
2015-03-26 01:46:09 +0900

    고침!!

--- stack_catalan/catalan.cpp
+++ stack_catalan/catalan.cpp
@@ -2,32 +2,44 @@
 
 int main(){
 	int num;
+	int i = 1;
 	int arr[101];
-	int arr_stack[102];
+	int stack[102];
 	int addr_stack=-1;
 	int compare = 1;
 	scanf("%d",&num);
 	for(int i=1;i<=num;i++)
 		scanf("%d",&arr[i]);
-	for(int i=1;i<=num;i++){
-		if(addr_stack == -1){
+	while(true){
+		if(addr_stack == -1 && i!=num+1){
 			addr_stack++;
-			arr_stack[addr_stack] = i;
-			printf("flag1\n");
+			stack[addr_stack] = i;
+			i++;
+			///printf("flag1\n");
 		}
-		else if(arr_stack[addr_stack] < arr[compare]){
+		else if(stack[addr_stack] < arr[compare]){
 			addr_stack++;
-			arr_stack[addr_stack] = i;
-			printf("flag2\n");
+			stack[addr_stack] = i;
+			i++;
+			//printf("flag2\n");
 		}
-		else if(arr_stack[addr_stack] == arr[compare]){
+		else if(stack[addr_stack] == arr[compare]){
 			addr_stack--;
 			compare++;
+			//printf("flag3\n");
+			//printf("i = %d\n",i);
+			//printf("addr_stack = %d\n",addr_stack);
+			//printf("stack = %d\n",stack[addr_stack]);
+			//printf("compare : %d, arr[%d] : %d\n",compare,compare,arr[compare]);
+			if(addr_stack == -1 && i==num+1){
+				printf("YES");
+				return 0;
+			}
 		}
-		else if(addr_stack == 0 && i==num){
-			printf("YES");
-			return 0;
-		}
+		//else if(addr_stack == -1 && i==num+1){
+		//	printf("YES");
+		//	return 0;
+		//}
 		else{
 			printf("NO");
 			return 0;
