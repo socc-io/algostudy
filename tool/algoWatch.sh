7d0980b786d290ea589ae7b8b094a7a379937631
HP100 <becxer87@gmail.com>
2015-06-24 22:33:49 +0900

    Create algoWatch.sh

---
+++ tool/algoWatch.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# auto generate algorithm initial file and watching 
+#
+# @email becxer87@gmail.com
+# @author becxer
+#
+
+if [ $# -lt 2 ]
+then
+	echo "usage: $0 title langauge"
+	echo "support language : cpp, CPP "
+	exit
+fi
+
+title=$1
+plang=$2
+cmd=""
+case "$plang" in
+cpp|CPP|Cpp) python genAlgoCPP.py $title
+			 cmd="python ./pydemon.py 'g++ $title.cpp -o $title.o;./$title.o $title.input'"
+	;;
+esac
+
+eval $cmd
