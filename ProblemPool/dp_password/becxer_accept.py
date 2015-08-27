679d1e58ef114f561a88d5c364f37fbf76fbbb3a
HP100 <becxer87@gmail.com>
2015-08-27 18:49:47 +0900

    Update becxer_accept.py

--- ProblemPool/dp_password/becxer_accept.py
+++ ProblemPool/dp_password/becxer_accept.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-arr = [[0 for i in range(10)] for i in xrange(1001)]
+arr = [[0 for i in xrange(10)] for i in xrange(1001)]
 arr[0] = [1 for i in xrange(10)]
 for j in xrange(1,1001):
 	for p in xrange(10):
@@ -11,7 +11,6 @@ for j in xrange(1,1001):
 			arr[j][p] += arr[j-1][p+3]
 		if p - 3 > -1:
 			arr[j][p] += arr[j-1][p-3]
-	arr[j] = map(lambda x: x % 1234567, arr[j])
 testcase = int(raw_input())
 for i in xrange(0,testcase):
 	n = int(raw_input())
