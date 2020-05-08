0f35e1f2e882576fa4c838c2708a52ad490bf3ca
smilu97 <smilup2244@gmail.com>
2020-05-08 23:00:16 +0900

    baekjoon.9244: line sweeping

--- baekjoon/smilu/9244.cpp
+++ baekjoon/smilu/9244.cpp
@@ -1,58 +1,116 @@
 #include <cstdio>
+#include <cassert>
 #include <vector>
+#include <set>
 #include <algorithm>
 using namespace std;
 
 typedef long long ll;
 
-const ll INF = 0x3f3f3f3f3f3f3f3fll;
-
 enum { LEFT, RIGHT };
-enum { LU, LD, RU, RD };
+enum { RU, RD, LD, LU };
 
 struct Point {
   ll x, y;
   Point() {}
-  Point(ll x, ll, y): x(x), y(y) {}
+  Point(ll x, ll y): x(x), y(y) {}
   void input() { scanf("%lld%lld", &x, &y); }
-};
-
-struct Event {
-  Point p;
-  int type;
-  int par;
-  Event() {}
-  void init(const Line &line, bool left) {
+  Point operator-(const Point &p) const {
+    return Point(x - p.x, y - p.y);
+  }
+  ll cross(const Point &p) const {
+    return x * p.y - y * p.x;
   }
 };
 
 struct Line {
   Point l, r;
+  int index;
   int type;
-  int next;
+  void update_type() { type = l.y < r.y ? LEFT : RIGHT; }
+  void input(int i) {
+    l.input();
+    r.input();
+    if (l.x > r.x) swap(l, r);
+    index = i;
+    update_type();
+  }
+  ll below_x() { return type == LEFT ? l.x : r.x; }
+  bool between(const Point &p) const { return l.x <= p.x && p.x <= r.x; }
+  bool below(const Point &p) const { return (r - l).cross(p - l) < 0; }
+  bool operator<(const Line &b) const {
+    if (index == b.index) return false;
+    if (between(b.l)) return below(b.l);
+    if (b.between(l)) return !b.below(l);
+    assert(false);
+    return index < b.index;
+  }
+  bool operator==(const Line &b) const { return index == b.index; }
   Line() {}
-  Line(const Point &l, const Point &r): l(l), r(r) {}
-  void input() { l.input(); r.input(); }
-  void update_type() {
-    type = l.y < r.y ? LEFT : RIGHT;
+  Line(int i, const Point &l, const Point &r): l(l), r(r), index(i) {
+    update_type();
   }
 };
 
-int n;
-Line lines[100000];
-Event events[200000];
+struct Event {
+  ll x;
+  int type;
+  int par;
+  Event() {}
+  void init(const Line &line, bool left) {
+    par = line.index;
+    x = left ? line.l.x : line.r.x;
+    if (left) {
+      type = line.type == LEFT ? LD : LU;
+    } else {
+      type = line.type == LEFT ? RU : RD;
+    }
+  }
+  bool operator<(const Event &e) const {
+    if (x == e.x) return type > e.type;
+    return x < e.x;
+  }
+};
 
-void get_input() {
-  scanf("%d", &n);
-  for (int i = 0; i < n; i++) lines[i].input();
-  ll sx; scanf("%lld", &sx);
-  lines[n] = Line(Point(sx-1, INF), Point(sx, INF-1));
-  ++n;
-  
-  for (int i = 0; i < n; i++) lines[i].update_type();
-  
-}
+int n, en;
+Line lines[100010];
+Event events[200020];
+int nxt[100010];
 
 int main(void)
 {
+  scanf("%d", &n);
+  for (int i = 0; i < n; i++) lines[i].input(i);
+  ll sx; scanf("%lld", &sx);
+  lines[n] = Line(n, Point(sx-1, 1000002), Point(sx, 1000001));
+  ++n; en = n<<1;
+  for (int i = 0; i < n; i++) {
+    events[ i<<1   ].init(lines[i], true);
+    events[(i<<1)|1].init(lines[i], false);
+  }
+  sort(events, events + en);
+  set<Line> scope;
+  for (int i = 0; i < n; i++) nxt[i] = -2;
+  for (int i = 0; i < en; i++) {
+    const Event &e = events[i];
+    Line &l = lines[e.par];
+    // printf("event { x: %lld, l: (%lld,%lld), r: (%lld,%lld) }\n", e.x, l.l.x, l.l.y, l.r.x, l.r.y);
+    if (e.type == LU) {
+      scope.insert(l);
+    } else if (e.type == LD) {
+      scope.insert(l);
+      auto it = scope.find(l); it++;
+      nxt[l.index] = it == scope.end() ? -1 : it->index;
+    } else if (e.type == RU) {
+      scope.erase(l);
+    } else { // RD
+      auto it = scope.find(l); it++;
+      nxt[l.index] = it == scope.end() ? -1 : it->index;
+      scope.erase(l);
+    }
+  }
+  int cur = n-1;
+  while (nxt[cur] >= 0) cur = nxt[cur];
+  assert(nxt[cur] != -2);
+  printf("%lld\n", lines[cur].below_x());
 }
