2fc530ad972fde71d0c41a05ba5552d066e99669
smilu97 <smilup2244@gmail.com>
2020-06-03 23:45:57 +0900

    baekjoon.15994: extended-euclid

--- baekjoon/smilu/15994.cpp
+++ baekjoon/smilu/15994.cpp
@@ -1,145 +1,110 @@
-#include <cstdio>
-#include <queue>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-#define MAXN 50000000
-#define MAXM 100
+typedef long long ll;
+typedef pair<ll, ll> pl;
+typedef pair<int, int> pi;
 
-// #define DEBUG
-
-struct Elev {
-    int x; // From floor
-    int y; // mult floor
-    int b; // route-back
-    int d;
-    /*
-     * Color flag-value
-     * If 1) begin elev
-     * If 2) end elev
-     * If 3) begin, also end elev
-     */
-    int c;
-    void input() {
-        scanf("%d%d", &x, &y);
-        --x;
-    }
-};
-
-int N, M, A, B;
-Elev elev[MAXM];
-int C[MAXM][MAXM];
-
-int min_dist, begin_elev;
-
-inline void input()
-{
-    scanf("%d%d", &N, &M);
-    for (int i = 0; i < M; i++) {
-        elev[i].input();   
-    }
-    scanf("%d%d", &A, &B);
-    --A, --B;
+ll extended_gcd(ll a, ll b, ll *x, ll *y) {
+  if (a == 0) {
+    *x = 0;
+    *y = 1;
+    return b;
+  }
+  ll x1, y1;
+  ll gcd = extended_gcd(b % a, a, &x1, &y1);
+  *x = y1 - (b/a) * x1;
+  *y = x1;
+  return gcd;
 }
 
-inline void output()
-{
-#ifdef DEBUG
-    printf("begin_elev: %d\n", begin_elev);
-#endif
-    int cur = begin_elev;
-    printf("%d\n%d\n", elev[begin_elev].d + 1, begin_elev + 1);
-    while ((elev[cur].c & 2) == 0) {
-        cur = elev[cur].b;
-        printf("%d\n", cur + 1);
-    }
+ll n, m;
+ll x[101], y[101];
+vector<int> adj[101];
+int dist[101];
+int bk[101];
+
+bool check_conn(int p, int q) {
+  if (y[p] > y[q]) swap(p, q);
+  ll a, b;
+  ll g = extended_gcd(y[p], y[q], &a, &b);
+  ll t = (n-x[q])/y[q]*y[q] + x[q] - x[p];
+  if (t < 0) return false;
+  if (t % g) return false;
+  a *= t/g; b *= t/g;
+
+  ll k = y[q]/g;
+  k = (k-a-1)/k;
+  a += k * y[q]/g;
+  b -= k * y[p]/g;
+
+  return b >= 0;
 }
 
-int calc_conn(int a, int b);
-inline void calc_conn_color()
-{
-    for (int i = 0; i < M; i++) {
-        int x = elev[i].x, y = elev[i].y;
-        if (x <= A && (A - x) % y == 0) {
-            elev[i].c |= 1;
-#ifdef DEBUG
-            printf("Elev %d is START\n", i);
-#endif
-        }
-        if (x <= B && (B - x) % y == 0) {
-            elev[i].c |= 2;
-#ifdef DEBUG
-            printf("Elev %d is END\n", i);
-#endif
-        }
-    }
-    for (int i = 0; i < M; i++) {
-        for (int j = i + 1; j < M; j++) {
-            C[i][j] = calc_conn(i, j);
-            C[j][i] = C[i][j];
-        }
-    }
+bool check_including(ll x, ll y, ll v) {
+  ll c = v - x;
+  if (c < 0) return false;
+  return (c % y) == 0;
 }
 
-inline int find_min_dist()
-{
-    queue<int> q;
-    for (int i = 0; i < M; i++) {
-        if (elev[i].c & 2) {
-            q.push(i);
-            elev[i].d = 0;
-        } else {
-            elev[i].d = -1;
-        }
+int main() {
+  ios::sync_with_stdio(0); cin.tie(0);
+  cin >> n >> m;
+  for (int i = 0; i < m; i++) cin >> x[i] >> y[i];
+  for (int i = 0; i < m; i++) {
+    for (int j = i+1; j < m; j++) {
+      if (check_conn(i, j)) {
+        adj[i].push_back(j);
+        adj[j].push_back(i);
+        // cout << "conn: " << i+1 << ' ' << j+1 << '\n';
+      }
     }
-    while(!q.empty()) {
-        int u = q.front();
-        q.pop();
-#ifdef DEBUG
-        printf("Visited %d\n", u);
-#endif
-        for (int v = 0; v < M; v++) {
-            if (C[u][v] == 0) continue;
-            if (elev[v].d != -1) continue;
-#ifdef DEBUG
-            printf("    Found %d\n", v);
-#endif
-            elev[v].d = elev[u].d + 1;
-            elev[v].b = u;
-            q.push(v);
-            if (elev[v].c & 1) {
-                begin_elev = v;
-                return 1;
-            }
-        }
+  }
+  ll s, e; cin >> s >> e;
+  priority_queue<pi, vector<pi>, greater<pi>> pq;
+  vector<bool> vis(m);
+  for (int i = 0; i < m; i++) {
+    dist[i] = check_including(x[i], y[i], e) ? 0 : 0x3f3f3f3f;
+    bk[i] = i;
+    if (!dist[i]) {
+      pq.push({0, i});
+      // cout << "start " << i << '\n';
     }
+  }
+  while (!pq.empty()) {
+    auto t = pq.top(); pq.pop();
+    int ud = t.first, u = t.second;
+    if (vis[u]) continue;
+    vis[u] = true;
+    for (int v: adj[u]) {
+      int vd = ud + 1;
+      if (vd < dist[v]) {
+        dist[v] = vd;
+        bk[v] = u;
+        pq.push({vd, v});
+      }
+    }
+  }
+  int min_dist = 0x3f3f3f3f;
+  int min_idx = -1;
+  for (int i = 0; i < m; i++) {
+    if (check_including(x[i], y[i], s)) {
+      if (dist[i] < min_dist) {
+        min_dist = dist[i];
+        min_idx = i;
+      }
+      // cout << "end " << i << '\n';
+    }
+  }
+  if (min_dist == 0x3f3f3f3f) {
+    cout << "-1\n";
     return 0;
-}
-
-int main(void)
-{
-#ifdef DEBUG
-    printf("Debug mode activated\n");
-#endif
-    input();
-    calc_conn_color();
-    find_min_dist();
-    output();
-
-    return 0;
-}
-
-int calc_conn(int a, int b)
-{
-    for (int i = 0; i < elev[b].y; i++) {
-        int v = elev[a].x + i * elev[a].y;
-        if (v >= N) return 0;
-        if ((v - elev[b].x) % elev[b].y == 0) {
-#ifdef DEBUG
-            printf("(%d, %d) is connected\n", a, b);
-#endif
-            return 1;
-        }
-    }
-    return 0;
+  }
+  cout << min_dist+1 << '\n';
+  int cur = min_idx;
+  while (bk[cur] != cur) {
+    cout << cur+1 << '\n';
+    cur = bk[cur];
+  }
+  cout << cur+1 << '\n';
 }
