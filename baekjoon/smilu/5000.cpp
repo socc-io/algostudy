bd3693acaaf087a802be97043cfb7318c33921b4
smilu97 <smilup2244@gmail.com>
2020-06-06 20:50:26 +0900

    baekjoon.5000: bit, constructive

--- baekjoon/smilu/5000.cpp
+++ baekjoon/smilu/5000.cpp
@@ -1,54 +1,44 @@
-#include <iostream>
-#include <map>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-int N;
-int arr[100000];
-int indices[100000];
+typedef pair<int, int> pi;
+typedef long long ll;
 
-void throw_bread(int pos)
-{
-	int tmp = arr[pos+2];
-	arr[pos+2] = arr[pos+1];
-	arr[pos+1] = arr[pos]; 
-	arr[pos] = tmp;
+const int MAX_N = 100010;
+
+struct BIT {
+  ll ft[MAX_N];
+  void update(int x, ll v) {
+    for (int i = x; i <= MAX_N; i+=(i&-i)) ft[i] += v;
+  }
+  ll query(int x) {
+    ll ret = 0;
+    for (int i = x; i > 0; i-=(i&-i)) ret += ft[i];
+    return ret;
+  }
+  ll query(int a, int b) {
+    return query(b) - query(a-1);
+  }
+} bit;
+
+int main() {
+	ios::sync_with_stdio(0); cin.tie(0);
+	int n; cin >> n;
+	vector<int> a(n);
+	vector<pi> b(n);
+	for (int i = 0; i < n; i++) cin >> a[i];
+	for (int i = 0; i < n; i++) {
+		cin >> b[i].first;
+		b[i].second = i + 1;
+	}
+	sort(b.begin(), b.end());
+	ll cnt = 0;
+	for (int i = 0; i < n; i++) {
+		a[i] = lower_bound(b.begin(), b.end(), make_pair(a[i], -1))->second;
+		cnt += bit.query(a[i] + 1, n);
+		bit.update(a[i], 1);
+	}
+	cout << ((cnt & 1) ? "Impossible" : "Possible") << '\n';
 }
 
-int main(void)
-{
-	map<int, int> pos;
-	scanf("%d", &N);
-
-	for(int i=0; i<N; ++i) {
-		int tmp;
-		scanf("%d", &tmp);
-		pos[tmp] = i;
-	}
-
-	for(int i=0; i<N; ++i) {
-		int tmp;
-		scanf("%d", &tmp);
-		arr[pos[tmp]] = i;
-		indices[i] = pos[tmp];
-	}
-
-	for(int i=0; i < N - 2; ++i) {
-		int cur = N - 1;
-		while(arr[cur] != i) --cur;
-		while(cur-2 >= i) {
-			throw_bread(cur-2);
-			cur -= 2;
-		}
-		if(cur != i) {
-			throw_bread(i);
-			throw_bread(i);
-		}
-	}
-
-	if(arr[N-1] == N-1) {
-		puts("Possible");
-	} else {
-		puts("Impossible");
-	}
-}
+// 4 2 1 3
