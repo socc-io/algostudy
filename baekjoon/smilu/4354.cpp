8f723c6e355e110729c2f024b33c8744591aee26
smilu97 <smilup2244@gmail.com>
2020-06-06 20:50:17 +0900

    baekjoon.4354: kmp

--- baekjoon/smilu/4354.cpp
+++ baekjoon/smilu/4354.cpp
@@ -1,49 +1,31 @@
-#include<iostream>
-#include<string>
-#include<vector>
-#include<algorithm>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-    while(1) {
-        string s;
-        cin >> s;
-        if(s[0] == '.') break;
-        int n = s.size();
-        vector<int> primes;
-        vector<int> n_primes;
-        for(int i=1; i*i <= n; ++i) {
-            if(n%i != 0) continue;
-            int na = n/i;
-            primes.push_back(i);
-            if(i*i != n) n_primes.push_back(na);
-        }
-        reverse(n_primes.begin(), n_primes.end());
-        primes.insert(primes.end(), n_primes.begin(), n_primes.end());
-        int ans;
-        for(auto it = primes.begin(); it != primes.end(); ++it) {
-            int i = *it;
-            int na = n/i;
-            bool fail = false;
-            for(int j=i; j<n; j+=i) {
-                bool same = true;
-                for(int k=0; k<i; ++k) {
-                    if(s[k] != s[j+k]) {
-                        same = false;
-                        break;
-                    }
-                }
-                if(!same) {
-                    fail = true;
-                    break;
-                }
-            }
-            if(!fail) {
-                ans = na;
-                break;
-            }
-        }
-        printf("%d\n", ans);
-    }
+typedef vector<int> vi;
+
+vi get_pi(const string &s) {
+  int n = s.size(), j = 0;
+  vi pi(n, 0);
+  for (int i = 1; i < n; i++) {
+    while (j > 0 && s[i] != s[j]) j = pi[j-1];
+    if (s[i] == s[j]) pi[i] = ++j;
+  }
+  return pi;
+}
+
+int solve(const string &s) {
+  vi pi = get_pi(s);
+  int n = s.length();
+  // cout << "pi: "; for (int i = 0; i < n; i++) cout << pi[i] << ' '; cout << '\n';
+  if (pi[n-1] % (n - pi[n-1])) return 1;
+  return n / (n - pi[n-1]);
+}
+
+int main() {
+  while (1) {
+    string s;
+    cin >> s;
+    if (s == ".") break;
+    cout << solve(s) << '\n';
+  }
 }
