03c5eb16dd8850a879242bea3d3e6b41b51c3aab
smilu97 <smilup2244@gmail.com>
2020-06-07 20:28:03 +0900

    baekjoon.6086: max-flow

--- baekjoon/smilu/6086.cpp
+++ baekjoon/smilu/6086.cpp
@@ -1,97 +1,103 @@
-#include <cstdio>
-#include <vector>
-
-#include <queue>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-int N;
+typedef long long ll;
 
-char c_tmp1, c_tmp2;
-int tmp;
+const int MAXN = 222;
+struct maxflow{
+	struct edg{int pos, cap, rev, idx;};
+	vector<edg> gph[MAXN];
+ 
+	void clear(){
+		for(int i=0; i<MAXN; i++){
+			gph[i].clear();
+		}
+	}
+ 
+	void add_edge(int s, int e, int x){
+		gph[s].push_back({e, x, (int)gph[e].size(), -1});
+		gph[e].push_back({s, 0, (int)gph[s].size()-1, -1});
+	}
+ 
+	void add_edge(int s, int e, int x, int idx){
+		gph[s].push_back({e, x, (int)gph[e].size(), idx});
+		gph[e].push_back({s, 0, (int)gph[s].size()-1, -1});
+	}
+ 
+	int dis[MAXN], pnt[MAXN];
+ 
+	bool bfs(int src, int sink){
+		memset(dis, 0, sizeof(dis));
+		memset(pnt, 0, sizeof(pnt));
+		queue<int> que;
+		que.push(src);
+		dis[src] = 1;
+		while(!que.empty()){
+			int x = que.front();
+			que.pop();
+			for(int i=0; i<gph[x].size(); i++){
+				edg e = gph[x][i];
+				if(e.cap > 0 && !dis[e.pos]){
+					dis[e.pos] = dis[x] + 1;
+					que.push(e.pos);
+				}
+			}
+		}
+		return dis[sink] > 0;
+	}
+ 
+	int dfs(int x, int sink, int f){
+		if(x == sink) return f;
+		for(; pnt[x] < gph[x].size(); pnt[x]++){
+			edg e = gph[x][pnt[x]];
+			if(e.cap > 0 && dis[e.pos] == dis[x] + 1){
+				int w = dfs(e.pos, sink, min(f, e.cap));
+				if(w){
+					gph[x][pnt[x]].cap -= w;
+					gph[e.pos][e.rev].cap += w;
+					return w;
+				}
+			}
+		}
+		return 0;
+	}
+ 
+	ll match(int src, int sink){
+		ll ret = 0;
+		while(bfs(src, sink)){
+			int r;
+			while((r = dfs(src, sink, 2e9))) ret += r;
+		}
+		return ret;
+	}
+} mf;
 
-vector< vector< pair<char, int> > > edges;
+int adj_map[52][52];
 
-inline int convertNameToIndex(char ch)
-{
-	if('a' <= ch && ch <= 'z') return ch - 'a' + 26;
+int index(char ch) {
+	if ('a' <= ch && ch <= 'z') {
+		return ch - 'a' + 26;
+	}
 	return ch - 'A';
 }
 
-int main(void)
-{
-	int N;
-	scanf("%d", &N);
-	edges.resize(52);
-	for(int i = 0; i < N; ++i){
-		c_tmp1 = convertNameToIndex(getchar()); getchar();
-		c_tmp2 = convertNameToIndex(getchar()); getchar();
-		scanf("%d", &tmp);
-		edges[c_tmp1].push_back(make_pair(c_tmp2, tmp));
-		edges[c_tmp2].push_back(make_pair(c_tmp1, 0));
+int main() {
+	ios::sync_with_stdio(0); cin.tie(0);
+
+	int n; cin >> n;
+	for (int i = 0; i < n; i++) {
+		char a, b; cin >> a >> b;
+		int c; cin >> c;
+		int p = index(a), q = index(b);
+		adj_map[p][q] += c;
+		adj_map[q][p] += c;
 	}
-
-	int cap_sum = 0;
-
-	while(true) {
-		printf("loop!\n");
-		queue<int> Q;
-		vector<int> tracker(52, -1);
-
-		Q.push(0);
-
-		bool found = false;
-		while(!Q.empty()) {
-			int u = Q.front(); Q.pop();
-			for(auto it = edges[u].begin(); it == edges[u].end(); ++it) {
-				int v = it->first;
-				int cap = it->second;
-				if(cap <= 0) continue;
-				if(tracker[v] != -1) continue;
-				tracker[v] = u;
-				Q.push(v);
-
-				if(v == 25) {
-					found = true;
-					break;
-				}
-			}
-			if(found) break;
-		}
-
-		if(tracker[25] == -1) break;
-
-		int min_cap = 0x7FFFFFFF;
-		for(int cur = 25; cur != 0; cur = tracker[cur]) {
-			int u = tracker[cur];
-			int v = cur;
-			for(auto it = edges[u].begin(); it != edges[u].end(); ++it) {
-				if(it->first == v) {
-					int cap = it->second;
-					if(min_cap > cap) min_cap = cap;
-					break;
-				}
-			}
-		}
-		cap_sum += min_cap;
-
-		for(int cur = 25; cur != 0; cur = tracker[cur]) {
-			int u = tracker[cur];
-			int v = cur;
-			for(auto it = edges[u].begin(); it != edges[u].end(); ++it) {
-				if(it->first == v) {
-					it->second -= min_cap;
-					break;
-				}
-			}
-			for(auto it = edges[v].begin(); it != edges[v].end(); ++it) {
-				if(it->first == u) {
-					it->second += min_cap;
-					break;
-				}
-			}
+	for (int i = 0; i < 52; i++) {
+		for (int j = i+1; j < 52; j++) {
+			if (adj_map[i][j] == 0) continue;
+			mf.add_edge(i, j, adj_map[i][j]);
+			mf.add_edge(j, i, adj_map[i][j]);
 		}
 	}
-
-	printf("%d", cap_sum);
+	cout << mf.match(0, 25) << '\n';
 }
