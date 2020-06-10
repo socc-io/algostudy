a688cdb4cf28da05a0c39dd210ffc571150ff225
smilu97 <smilup2244@gmail.com>
2020-06-11 00:27:37 +0900

    baekjoon.11758: ccw

--- baekjoon/smilu/11758.cpp
+++ baekjoon/smilu/11758.cpp
@@ -1,7 +1,21 @@
-#include<iostream>
+#include <bits/stdc++.h>
+using namespace std;
 
-int main()
-{
-    int x1, y1, x2, y2, x3, y3;
-    scanf("%d%d%d%d%d%d", &x1, &y1, &x2, &y2, &x3, &y3);
+typedef long long ll;
+typedef pair<ll, ll> pll;
 
+int ccw(pll a, pll b, pll c) {
+  ll op = a.first*b.second + b.first*c.second + c.first*a.second;
+  op -= (a.second*b.first + b.second*c.first + c.second*a.first);
+  if (op > 0) return 1;
+  else if (op == 0) return 0;
+  else return -1;
+}
+
+int main() {
+  pll a, b, c;
+  cin >> a.first >> a.second;
+  cin >> b.first >> b.second;
+  cin >> c.first >> c.second;
+  cout << ccw(a, b, c) << '\n';
+}
