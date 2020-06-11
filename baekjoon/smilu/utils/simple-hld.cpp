125994d828aa6e060af07c33e47df025f9168162
smilu97 <smilup2244@gmail.com>
2020-06-11 17:01:50 +0900

    add util.simple-hdd

---
+++ baekjoon/smilu/utils/simple-hld.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void init_sz(
+  const vector<vector<int>> &adj,
+  vector<int> &sz,
+  int u,
+  int p
+) {
+  sz[u] = 1;
+  for (int &v: adj[u]) if (v != p) {
+    init_sz(adj, sz, v, u);
+    sz[u] += sz[v];
+    if (sz[u] > sz[adj[u][0]]) swap(v, adj[u][0]);
+  }
+}
+
+void init_hld(
+  const vector<vector<int>> &adj,
+  const vector<int> &sz,
+  vector<int> &in,
+  vector<int> &top,
+  int u,
+  int p
+) {
+  int piv = in[u] + 1;
+  for (int v: adj[u]) if (v != p) {
+    in[v] = piv;
+    piv += sz[v];
+    top[v] = (v == adj[u][0] ? top[u] : v);
+    init_hld(adj, sz, in, top, v, u);
+  }
+}
