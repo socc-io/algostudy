14c393b2a687150a926d819ba244a108b9208e4f
smilu97 <smilup2244@gmail.com>
2020-06-16 19:37:33 +0900

    add util.bipartite-matching

---
+++ baekjoon/smilu/utils/bipartite-matching.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class BipartiteMatch {
+  int n;
+  vector<vector<int>> adj;
+  vector<int> mch;
+  vector<bool> lck;
+  bool augment(int u) {
+    if (lck[u]) return false;
+    lck[u] = true;
+    for (int v: adj[u]) {
+      if (mch[v] == -1 || augment(mch[v])) {
+        mch[v] = u;
+        return true;
+      }
+    }
+    return false;
+  }
+public:
+  BipartiteMatch(int n, int m): n(n), adj(n), mch(m), lck(n) {}
+  void add_edge(int u, int v) {
+    adj[u].push_back(v);
+  }
+  int execute() {
+    int ret = 0;
+    fill(mch.begin(), mch.end(), -1);
+    for (int i = 0; i < n; i++) {
+      fill(lck.begin(), lck.end(), false);
+      ret += augment(i);
+    }
+    return ret;
+  }
+};
