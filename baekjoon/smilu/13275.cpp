2003c864d3d9e74f4318b874485b0f6a5ff0234a
smilu97 <smilup2244@gmail.com>
2020-06-18 18:22:24 +0900

    baekjoon.13275: manacher

---
+++ baekjoon/smilu/13275.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> manacher_primitive(string s) {
+  int n = s.size();
+  vector<int> a(n);
+  int r = 0, p = 0;
+  for (int i = 0; i < n; i++) {
+    a[i] = i <= r ? min(a[2*p-i], r-i) : 0;
+    while (i-a[i]-1 >= 0 && i+a[i]+1 < n && s[i-a[i]-1] == s[i+a[i]+1]) ++a[i];
+    if (r < i+a[i]) {
+      r = i + a[i];
+      p = i;
+    }
+  }
+  return a;
+}
+
+vector<int> manacher(string s, char dummy = '.') {
+  int n = s.size();
+  string s2;
+  s2.resize(n*2+1);
+  s2[0] = dummy;
+  int idx = 1;
+  for (int i = 0; i < n; i++) {
+    s2[idx++] = s[i];
+    s2[idx++] = dummy;
+  }
+  // cout << s2 << endl; // debug
+  return manacher_primitive(s2);
+}
+
+int main() {
+  ios::sync_with_stdio(0); cin.tie(0);
+  string s; cin >> s;
+  const vector<int> pl = manacher(s);
+  cout << *max_element(pl.begin(), pl.end()) << '\n';
+}
