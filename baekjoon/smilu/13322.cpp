560404deb86f3e4e63d81e344b312081d1dd3ba3
smilu97 <smilup2244@gmail.com>
2020-06-27 17:02:38 +0900

    baekjoon.13322: 당했다

---
+++ baekjoon/smilu/13322.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(0); cin.tie(0);
+  string s; cin >> s;
+  const int n = s.size();
+  for (int i = 0; i < n; i++) cout << i << '\n';
+}
