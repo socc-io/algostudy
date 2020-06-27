6c33c4acb6c82c24b15ced96cd34a44020c3929a
smilu97 <smilup2244@gmail.com>
2020-06-27 23:31:25 +0900

    update util.mos

--- baekjoon/smilu/utils/mos.cpp
+++ baekjoon/smilu/utils/mos.cpp
@@ -1,7 +1,4 @@
-#include <cstdio>
-#include <cmath>
-#include <vector>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
 
 typedef pair<int,int> pii;
@@ -23,61 +20,46 @@ bool index_sort(const Query &a, const Query &b) {
 }
 
 int n, c, m;
-int cs[300000];
-Query ps[10000];
-int cnt[10000];
+int as[100010];
+Query ps[100010];
 
-void set_ans(Query& q) {
-  int th = (q.r - q.l) / 2;
-  for (int i = 0; i < 10000; i++) {
-    if (cnt[i] > th) {
-      q.ans = i;
-      return;
-    }
-  }
-  q.ans = -1;
-  return;
+int cnt[1000001];
+int s_cnt;
+void inc(int x) {
+  s_cnt += cnt[x] == 0;
+  ++cnt[x];
+}
+void dec(int x) {
+  s_cnt -= cnt[x] == 1;
+  --cnt[x];
 }
 
 int main(void)
 {
-  scanf("%d%d", &n, &c);
-  for (int i = 0; i < n; i++) {
-    scanf("%d", &cs[i]); --cs[i];
-  }
-  scanf("%d", &m);
+  ios::sync_with_stdio(0); cin.tie(0);
+  cin >> n;
+  for (int i = 1; i <= n; i++) cin >> as[i];
+  cin >> m;
   for (int i = 0; i < m; i++) {
-    scanf("%d%d", &ps[i].l, &ps[i].r);
     ps[i].index = i;
-    ps[i].l--;
+    cin >> ps[i].l >> ps[i].r;
   }
+  
   mo_size = sqrt(n);
   sort(ps, ps + m, mo_sort);
-
-  for (int i = ps[0].l; i < ps[0].r; i++) {
-    cnt[cs[i]]++;
-  }
-  set_ans(ps[0]);
   
-  int l = ps[0].l, r = ps[0].r;
-  for (int i = 1; i < m; i++) {
+  int l = ps[0].l, r = ps[0].l - 1;
+  for (int i = 0; i < m; i++) {
     int nl = ps[i].l;
     int nr = ps[i].r;
-    for (int i = l; i < nl; i++) cnt[cs[i]]--;
-    for (int i = l-1; i >= nl; i--) cnt[cs[i]]++;
-    for (int i = r; i < nr; i++) cnt[cs[i]]++;
-    for (int i = r-1; i >= nr; i--) cnt[cs[i]]--;
-    set_ans(ps[i]);
-    l = nl;
-    r = nr;
+    while (l < nl) dec(as[l++]);
+    while (l > nl) inc(as[--l]);
+    while (r < nr) inc(as[++r]);
+    while (r > nr) dec(as[r--]);
+    ps[i].ans = s_cnt;
   }
   sort(ps, ps + m, index_sort);
   for (int i = 0; i < m; i++) {
-    int ans = ps[i].ans;
-    if (ans == -1) {
-      puts("no");
-    } else {
-      printf("yes %d\n", ans + 1);
-    }
+    cout << ps[i].ans << '\n';
   }
 }
