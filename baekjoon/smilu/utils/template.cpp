aa3883267871d19abc772aac03a061a74cb97950
smilu97 <smilup2244@gmail.com>
2020-09-06 23:01:27 +0900

    Add PS-template.cpp

---
+++ baekjoon/smilu/utils/template.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+  
+}
+
+int main() {
+  ios::sync_with_stdio(0); cin.tie(0);
+  int t; cin >> t;
+  while (t--) {
+    solve();
+  }
+}
