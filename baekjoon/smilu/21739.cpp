5b57f3e8942e7d0a215b0d0cfacc3e91ca0b0819
smilu97 <smilup2244@gmail.com>
2021-05-18 00:56:26 +0900

    add 20943, 21739

---
+++ baekjoon/smilu/21739.cpp
@@ -0,0 +1,27 @@
+#include <cstdio>
+
+typedef long long ll;
+
+const ll mod = 1000000007;
+ll m[10010][10010];
+
+int inv(int x) {
+	int r, b = mod-2;
+	for (r = 1; b; b >>= 1) {
+		if ((b & 1)) r = (1ll * r * x) % mod;
+		x = (1ll * x * x) % mod;
+	}
+	return r;
+}
+
+int main() {
+	int n;
+	scanf("%d", &n);
+	int u = 1, d = 1;
+	for (int i = n+1; i <= (n<<1); i++)
+		u = (1ll * u * i) % mod;
+	for (int i = 1; i <= n; i++)
+		d = (1ll * d * i) % mod;
+	d = (1ll * d * (n+1)) % mod;
+	printf("%lld", (1ll * u * inv(d)) % mod);
+}
