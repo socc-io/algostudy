5600297e7fb5906b99907383b2ccec75c860ee2c
youngjin.kim2 <youngjin.kim2@navercorp.com>
2022-08-11 21:39:43 +0900

    smilu baekjoon 3651

--- baekjoon/smilu/3651.cpp
+++ baekjoon/smilu/3651.cpp
@@ -3,18 +3,20 @@ using namespace std;
 
 typedef long long lld;
 
-lld comb_cache[1000][50];
-
 lld comb(lld n, lld k) {
-    if (k > n) return 0ll;
-    if (k == 0) return 1ll;
-    int &r = comb_cache[n][k];
-    return r = comb(n, k-1) + comb(n-1, k-1);
+    lld r = 1;
+    for (lld i = 1; i <= k; i++) {
+        r *= n--;
+        r /= i;
+    }
+    return r;
 }
 
 lld search_km(lld k, lld m) {
     lld n_hi = 1;
-    while (comb(n_hi, k) < m) n_hi <<= 1;
+    while (comb(n_hi, k) < m) {
+        n_hi <<= 1;
+    }
 
     lld n_lo = n_hi >> 1;
 
@@ -42,22 +44,18 @@ lld search_km(lld k, lld m) {
 }
 
 int main(void) {
-    vector<pair<lld, lld>> ans;
-
     lld m; cin >> m;
 
-    ans.push_back(make_pair(m, 1));
-    ans.push_back(make_pair(m, m-1));
-
-    for (lld k = 2; k < 50; k++) {
+    vector<pair<lld, lld>> ans;
+    for (lld k = 1; k < 50; k++) {
         lld n = search_km(k, m);
         if (n == -1) continue;
         ans.push_back(make_pair(n, k));
+        ans.push_back(make_pair(n, n-k));
     }
 
     sort(ans.begin(), ans.end());
     ans.erase(unique(ans.begin(), ans.end()), ans.end());
-
     cout << ans.size() << '\n';
     for (auto p: ans) {
         cout << p.first << ' ' << p.second << '\n';
